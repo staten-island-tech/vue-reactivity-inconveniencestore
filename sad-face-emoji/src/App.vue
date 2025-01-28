e5ee3acca82f40554b13ce54dff7c5b6bbdec2d6
inconveniencestore <iivyleaves@gmail.com>
2025-01-28 12:44:03 -0500

    we're getting somewhere, which is nowhere

--- sad-face-emoji/src/App.vue
+++ sad-face-emoji/src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import TitleItem from './components/TitleItem.vue'
+import AvatarDisplay from './components/AvatarDisplay.vue'
 </script>
 
 <template>
-  <div>image goes here</div>
+  <header>
+    <div><TitleItem /></div>
+  </header>
+  <div>
+    <AvatarDisplay />
+  </div>
   <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <!-- <RouterLink to="/">Home</RouterLink> -->
+    <RouterLink to="/face">face</RouterLink>
+    <RouterLink to="/eyes">eyes</RouterLink>
+    <RouterLink to="/clothes">clothes</RouterLink>
+    <RouterLink to="/hair">hair</RouterLink>
+    <RouterLink to="/accessories">accessories</RouterLink>
   </nav>
   <RouterView />
 </template>
---
+++ sad-face-emoji/src/components/TitleItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <h1>Avatar Creator</h1>
+  </div>
+</template>
+
+<!-- <script setup></script> -->
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+}
+</style>
