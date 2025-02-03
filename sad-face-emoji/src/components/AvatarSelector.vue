d6f82b43e669cfe5aef6b8944592b948e99ea54c
inconveniencestore <iivyleaves@gmail.com>
2025-02-03 12:30:36 -0500

    emit works now gotta link it to the dispaly now

--- sad-face-emoji/src/components/AvatarSelector.vue
+++ sad-face-emoji/src/components/AvatarSelector.vue
@@ -2,16 +2,14 @@
   <div>
     <nav>
       <button @click="currentSection = 'hair'">Hair</button>
-      <button @click="$emit('toggle-checkbox')'">Eyes</button>
     </nav>
 
     <div v-if="currentSection === 'hair'">
       <CardDisplay
         v-for="hair in data"
-        :key="hair.id"
-        :id="hair.id"
         :src="hair.src"
         :selected="hair.selected"
+        @user-clicked="handleUserClick"
       ></CardDisplay>
     </div>
     <div v-else-if="currentSection === 'eyes'">
@@ -25,8 +23,15 @@
 
 <script setup>
 import { ref } from 'vue'
-import CardDisplay from '../views/CardDisplay.vue'
+import CardDisplay from './CardDisplay.vue'
 import data from '../data.js'
 
+//data is the term that i defined it as
+//its also the thing that's emitted. probably should define it as when its true
+const handleUserClick = (data) => {
+  alert(`User ${data} clicked!`)
+}
+//reactice can hange the array: put in seperate js file??
+//put stuff in as props
 const currentSection = ref('hair')
 </script>
---
+++ sad-face-emoji/src/components/CardDisplay.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps(['src', 'selected'])
+const emit = defineEmits('user-clicked')
+const selectItem = () => {
+  emit('user-clicked', props.src)
+}
+</script>
+
+<template>
+  <div class="item-holder">
+    <h4>{{ props.src }}</h4>
+    <img :src="props.src" alt="" />
+    <input type="checkbox" @click="selectItem" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  height: 10rem;
+  width: 10rem;
+  object-fit: contain;
+}
+.item-holder {
+  height: 12rem;
+  width: 11.5rem;
+  margin: 1rem;
+  background-color: rgb(123, 255, 0);
+}
+</style>
